51decb6c2fa8b5c5c5930e711f11015f480be4cc
kebohonganyangjujur <kebohonganyangjujur@gmail.com>
2022-07-03 14:31:47 +0700

    screen1 done (able to calculate BMI and showed the value in debugarea)

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSliderValue: UISlider!
+    @IBOutlet weak var heightSliderValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSliderValue.value
+        let weight = weightSliderValue.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+    
+    
 }
 
